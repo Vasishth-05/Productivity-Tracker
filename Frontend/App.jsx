671e4e17b336d6a5bb12cbb86a83923db039c8f3
Vasishth <learnvasishth@gmail.com>
2025-06-18 16:44:14 +0530

    added Task code

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -3,11 +3,18 @@
 // On load, it will fetch tasks from the backend and store them
 // It will pass the tasks and functions (like addTask, deleteTask) to child components
 
-import React, { useState } from 'react'
+import { response } from 'express';
+import React, { useEffect, useState } from 'react'
 
 export const TaskList = () => {
 
-    const [taskList,setTaskList] = useState([])
+    const [taskList,setTaskList] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/api/tasks`)
+        .then(response => response.json())
+        .catch(error => console.error(error))
+    }, [])
 
   return (
     <div>TaskList</div>
